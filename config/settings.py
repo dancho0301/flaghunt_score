89bbc39e1ebff3b5f487d61830ccef3da14fd2f0
dancho0301 <samuraimania.d@gmail.com>
2020-06-18 17:11:02 +0900

    STATIS_ROOTをコメントアウト

--- config/settings.py
+++ config/settings.py
@@ -108,18 +108,24 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = 'ja'
-
 TIME_ZONE = 'Asia/Tokyo'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIS_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIS_ROOT = os.path.join(BASE_DIR, 'static')
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
